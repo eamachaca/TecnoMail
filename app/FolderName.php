82b7813daaaa330c233773250895df6c2c81db35
Eduardo Machaca <eamachaca@hotmail.com>
2017-11-30 20:46:50 -0400

    applying changes to models for trigger simulator

--- app/FolderName.php
+++ app/FolderName.php
@@ -4,7 +4,15 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class NameMail extends Model
+class FolderName extends Model
 {
-    //
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = ['name'];
+    public $timestamps = false;
+
 }
