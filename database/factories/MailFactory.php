aa7fa47f5c064bfa4161284bef2fa20f731a04a2
DeIt0 <eduardito_145@hotmail.com>
2018-11-22 23:19:39 +0000

    emergency 3.0

--- database/factories/MailFactory.php
+++ database/factories/MailFactory.php
@@ -1,19 +1,12 @@
 <?php
-use WatsonSDK\Common\WatsonCredential;
-use WatsonSDK\Common\SimpleTokenProvider;
-use WatsonSDK\Services\NaturalLanguageUnderstanding;
-use WatsonSDK\Services\NaturalLanguageUnderstanding\AnalyzeModel;
+
 use Faker\Generator as Faker;
+
 $factory->define(\App\Mail::class, function (Faker $faker) {
     $user = \App\User::all()->random();
-    $realText=$faker->realText();
-	$nlu = new NaturalLanguageUnderstanding( WatsonCredential::initWithCredentials('initWithCredentials','*#DeIt0*#') );
-	$model = new AnalyzeModel($realText,[]);
-	$result = $nlu->analyze($model);
-	dd($result->getContent());
     return [
         'subject' => substr($faker->slug, 0, 50),
-        'body' => '<h1> hola ' . $realText . '</h1>',
+        'body' => '<h1> hola ' . $faker->realText() . '</h1>',
         'sended' => $faker->boolean(),
         'readed' => $faker->boolean(70),
         'user_id' => $user->id,
